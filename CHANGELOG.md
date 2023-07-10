1f4b9bfb52fa345b3ab7148f116a56f63b4904eb
Gareth Dawson <gwdawson.work@gmail.com>
2023-07-10 11:06:04 +0100

    make everything pretty

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,7 @@
-0.1.0 / 2022-06-17
-===================
+# 0.1.0 / 2022-06-17
 
-* **[Chore]**: Upgrade version to `0.1.0` to indicate the breaking change in a previous version.
+- **[Chore]**: Upgrade version to `0.1.0` to indicate the breaking change in a previous version.
 
-0.0.15 / 2022-06-17
-===================
+  # 0.0.15 / 2022-06-17
 
-* **[Chore]**: Upgrade `@lezer/lr` and `@lezer/generator` to their latest versions.
+- **[Chore]**: Upgrade `@lezer/lr` and `@lezer/generator` to their latest versions.
--- test/test-logql.js
+++ test/test-logql.js
@@ -5,12 +5,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 
-let caseDir = path.dirname(fileURLToPath(import.meta.url))
+let caseDir = path.dirname(fileURLToPath(import.meta.url));
 for (const file of fs.readdirSync(caseDir)) {
-    if (!/\.txt$/.test(file)) continue;
+  if (!/\.txt$/.test(file)) continue;
 
-    const name = /^[^\.]*/.exec(file)[0];
-    describe(name, () => {
-        for (const { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file)) it(name, () => run(parser));
-    });
+  const name = /^[^\.]*/.exec(file)[0];
+  describe(name, () => {
+    for (const { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file))
+      it(name, () => run(parser));
+  });
 }
